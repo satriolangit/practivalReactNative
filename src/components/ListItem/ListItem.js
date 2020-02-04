ed882a8205fce606c63f08ae8dd02e7bf4b064f4
SMI Developer <smidev@gmail.com>
2020-02-04 23:58:50 +0700

    done

---
+++ src/components/ListItem/ListItem.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {View, Text, StyleSheet, TouchableHighlight} from 'react-native';
+
+const ListItem = props => {
+  return (
+    <TouchableHighlight onPress={props.onItemPressed}>
+      <View style={styles.listItem}>
+        <Text>{props.placeName}</Text>
+      </View>
+    </TouchableHighlight>
+  );
+};
+
+const styles = StyleSheet.create({
+  listItem: {
+    width: '100%',
+    padding: 10,
+    backgroundColor: '#eee',
+    marginBottom: 5,
+  },
+});
+
+export default ListItem;
